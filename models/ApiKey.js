a5aa6ce68c095c579ddc34ffd0ae3b6e789df24a
OchusRukky <ochukorukky636@gmail.com>
2026-03-21 09:48:03 +0100

    Update ApiKey.js

--- models/ApiKey.js
+++ models/ApiKey.js
@@ -62,7 +62,7 @@ const apiKeySchema = new mongoose.Schema({
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: false
   },
   createdAt: {
     type: Date,
