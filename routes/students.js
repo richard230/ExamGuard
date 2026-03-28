19a8f483873ecdb57767d7a5c52c30e2ecc2ad55
Joan230647 <joanokosun0@gmail.com>
2026-03-28 14:44:21 +0100

    Update students.js

--- routes/students.js
+++ routes/students.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const studentAuthMiddleware = require('../middleware/studentAuth');
